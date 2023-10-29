9cd3b0a895a8d1e472e472963ae23e98d4702643
Karl Laurentius Roos <karlroos93@gmail.com>
2023-10-29 17:41:49 +0100

    docs: updated example mailer script and screenshot

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const mailer = nodemailer.createTransport({
   await mailer.sendMail({
     to: 'karl@yellowred.blue',
     from: 'CrabAlert <no-reply@yellowred.blue>',
-    subject: 'Crab Alert',
-    body: 'Crab Alert!',
-    html: 'Crab Alert!',
+    subject: 'Confirm your e-mail',
+    html: '\n<p>\n</p>\n <p>Test</p>\n <p>   To continue signing in to your account, please click <a href="#">this link</a>.</p><p>If you did not attempt to sign in you can ignore this e-mail.</p>',
   }).then(res => {
     console.log(res);
   }).catch(err => {
